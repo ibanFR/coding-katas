8a6316c8e3a54d03521f2c9339b3def57e380800
ibanFR <ibanfernandezrojo@msn.com>
2025-01-11 18:21:49 +0100

    Start Leap Year Kata
    Added test list

---
+++ src/main/java/com/ibanfr/leapyears/LeapYearCalculator.java
@@ -0,0 +1,9 @@
+package com.ibanfr.leapyears;
+
+public class LeapYearCalculator {
+
+
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0;
+    }
+}
---
+++ src/test/java/com/ibanfr/leapyears/LeapYearCalcTest.java
@@ -0,0 +1,28 @@
+package com.ibanfr.leapyears;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LeapYearCalcTest {
+
+
+    //TEST LIST
+    // [X] - leap year is divisible by 4
+    // [] - leap year is divisible by 4 but not by 100
+    // [] - leap year is divisible by 4 and 100 and 400
+    // [] - 1996 is a typical leap year
+    // [] - 2000 is an atypical leap year
+    // [] - 2001 is a typical common year
+    // [] - 1900 is an atypical common year
+
+
+    @Test
+    void leap_year_is_divisible_by_4() {
+
+        boolean isLeapYear = LeapYearCalculator.isLeapYear(4);
+
+        assertThat(isLeapYear)
+                .isTrue();
+    }
+}
